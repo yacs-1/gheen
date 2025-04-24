d2e4b104b06a4e451d3d7991fc44e4538d505913
yacs-1 <chromebook1@youngsvilleacademy.org>
2025-04-24 15:21:53 -0400

    Update global-header.js

--- global-header.js
+++ global-header.js
@@ -2,34 +2,7 @@
 this script will inject the global page header into the #gheader div of each page
 **/
 
-const INJECTION= "<div id="global-header"><ul id="nav-list"><li> Home</li><li><a href="#"> Photos</a>           <ul><li><a href="#">album 1 </a></li><li><a href="#">album 2 </a></li><li><a href="#">album 3 </a></li></ul></li><li><a href="#"> My Music </a>
-<ul>
-<li><a href="#">album 1 </a></li>
-<li><a href="#">album 2 </a></li>
-<li><a href="#">album 3 </a></li>
-</ul>
-</li>
-<li><a href="#"> Sports </a>
-<ul>
-<li><a href="#">album 1 </a></li>
-<li><a href="#">album 2 </a></li>
-<li><a href="#">album 3 </a></li>
-</ul>
-</li>
-<li> <a href="#">Web Dev </a>
-<ul>
-<li><a href="#">Week 1 </a></li>
-<li><a href="#">Week 2 </a></li>
-<li><a href="#">Week 3 </a></li>
-<li><a href="#">Week 4 </a></li>
-<li><a href="#">Week 5 </a></li>
-<li><a href="#">Week 6 </a></li>
-<li><a href="#">Week 7 </a></li>
-<li><a href="#">Week 8 </a></li>
-<li><a href="#">Week 9 </a></li>
-</ul>
-</li>
-</ul>
+const INJECTION= "<div id="global-header"><ul id="nav-list"><li> Home</li><li><a href="#"> Photos</a><ul><li><a href="#">album 1 </a></li><li><a href="#">album 2 </a></li><li><a href="#">album 3 </a></li></ul></li><li><a href="#"> My Music </a><ul><li><a href="#">album 1 </a></li><li><a href="#">album 2 </a></li><li><a href="#">album 3 </a></li></ul></li><li><a href="#"> Sports </a><ul><li><a href="#">album 1 </a></li><li><a href="#">album 2 </a></li><li><a href="#">album 3 </a></li></ul></li><li> <a href="#">Web Dev </a><ul><li><a href="#">Week 1 </a></li><li><a href="#">Week 2 </a></li><li><a href="#">Week 3 </a></li><li><a href="#">Week 4 </a></li><li><a href="#">Week 5 </a></li><li><a href="#">Week 6 </a></li><li><a href="#">Week 7 </a></li><li><a href="#">Week 8 </a></li><li><a href="#">Week 9 </a></li></ul></li></ul>
 </div>
   ";
     let_div= document.getElementById("gheader");
